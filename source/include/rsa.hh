b075b16db5fff1c1a83648c5e8efcb5169939782
lluchiari <luchiari.lucas@gmail.com>
2017-05-25 16:46:59 -0300

    Testing simultaneous commit

--- source/include/rsa.hh
+++ source/include/rsa.hh
@@ -1,10 +1,10 @@
 #ifndef _RSA_HH_
 #define _RSA_HH_
 
-// This is the DATA structure for the private key
+/* This is the DATA structure for the private key */
 typedef int DATA_PRIVATE;
 
-// This is the DATA structure for the private key
+/* This is the DATA structure for the private key */
 typedef int DATA_PUBLIC;
 
 class RSA
@@ -16,11 +16,16 @@ public:
     void gerateKey();
 
 private:
-    DATA_PRIVATE _privateKey;         //This is the private key gerated. The data type is
-    int _p, _q;
+    /*This function gerates the primes. Firs see a method*/
+    int geratePrime();
 
 public:
     DATA_PUBLIC publicKey;
+
+private:
+    /* This is the private key gerated. The data type is */
+    DATA_PRIVATE _privateKey;
+    int _p, _q;
 };
 
 #endif _RSA_HH_
--- source/src/rsa.cpp
+++ source/src/rsa.cpp
@@ -5,8 +5,19 @@ RSA::RSA()
 
 }
 
-RSA::gerateKey()
+void RSA::gerateKey()
 {
+    /* Gerating the High primes */
+    this->_p = geratePrime();
+    this->_q = geratePrime();
+
+    /* Calculating Paramters based on gerated primes */
+    int n, x;
+    n = _p*_q;
+    x = (_p-1)(_q-1);
+
+    /* Calculate the Gratest Common Divisor */
+    gdc();
 
 }
 
